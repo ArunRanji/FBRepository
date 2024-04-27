52166928b41e6d7cbddf34159db0cce4295bd209
ArunRanji <165883713+ArunRanji@users.noreply.github.com>
2024-04-27 16:55:29 +0530

    Create MergeConflicts.java

---
+++ src/test/java/demo/flipkart/com/MergeConflicts.java
@@ -0,0 +1,10 @@
+package demo.flipkart.com;
+
+public class MergeConflicts {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is merge conflicts class - remote repo");
+	}
+
+}
